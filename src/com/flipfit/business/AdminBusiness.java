0477e4eee628f842ab56a31583806babe4d2bf13
ElCapitan47 <yashvardhanshah7@gmail.com>
2025-07-22 23:33:17 +0530

    first commit

--- src/com/flipfit/business/AdminBusiness.java
+++ src/com/flipfit/business/AdminBusiness.java
@@ -4,6 +4,7 @@ import com.flipfit.bean.Gym;
 import com.flipfit.bean.GymOwner;
 import com.flipfit.bean.GymUser;
 
+//Checking for changes
 public class AdminBusiness
 {
     private boolean approval(int GymId)
